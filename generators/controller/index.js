f327fef12f6fc809fbeb2368daba5fb0c0d33068
Atef Haque <atefth@gmail.com>
2016-05-24 13:22:01 +0800

    fixed bugs with composer async task sequence

--- generators/controller/index.js
+++ generators/controller/index.js
@@ -2,9 +2,11 @@
 var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
+var wiring = require('html-wiring');
 
 module.exports = yeoman.generators.Base.extend({
   init: function () {
+    this.appName = this.config.get('appName');
   },
   prompting: function () {
     var done = this.async();
@@ -19,8 +21,7 @@ module.exports = yeoman.generators.Base.extend({
       this.props = props;
       // To access props later use this.props.someOption;
       this.controllerName = this.props.controller;
-      this.controllerDir = this.destinationRoot()+'/app/controllers';
-      this.destinationRoot(this.controllerDir);
+      this.controllerDir = this.destinationRoot()+'/public/client/app/controllers';
       this.log(yosay(chalk.red(this.controllerName)));
 
       done();
@@ -29,11 +30,17 @@ module.exports = yeoman.generators.Base.extend({
 
   writing: {
     controller: function () {
+      this.destinationRoot(this.controllerDir);
       this.fs.copyTpl(
         this.templatePath('_Controller.js'),
         this.destinationPath(this.controllerName+'Ctrl.js'),
-        { name: this.config.get('appName'), controller: this.controllerName }
+        { name: this.appName, controller: this.controllerName }
         );
+      var hook = 'generated-controllers';
+      this.destinationRoot(this.destinationRoot()+'/../../../../');
+      var path = this.destinationPath('/resources/views/welcome.blade.php');
+      var insert = '\n<script src="client/app/controllers/'+this.controllerName+'Ctrl.js" type="text/javascript" charset="utf-8"></script>\n';
+      wiring.appendToFile(path, hook, insert);
     }
   },
 
