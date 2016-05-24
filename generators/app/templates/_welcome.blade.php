df6920fc17685376c396c7abdd59cf95dbaec5a9
Atef Haque <atefth@gmail.com>
2016-05-24 13:26:16 +0800

    fixed async task sequence
    Fixed composer install task running asynchronously

--- generators/app/templates/_welcome.blade.php
+++ generators/app/templates/_welcome.blade.php
@@ -1,26 +1,30 @@
 <!DOCTYPE html>
 <html ng-app="<%= name %>">
-	<head>
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<title><%= name %></title>
-		<link rel="stylesheet" type="text/css" href="client/assets/css/app.css">
-		<link rel="stylesheet" type="text/css" href="client/vendor/bootstrap/dist/css/bootstrap.min.css">
-	</head>
-	<body>
-		
-		<div class="container">
-			<div ui-view> </div>
-		</div>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title><%= name %></title>
+	<link rel="stylesheet" type="text/css" href="client/assets/css/app.css">
+	<link rel="stylesheet" type="text/css" href="client/vendor/bootstrap/dist/css/bootstrap.min.css">
+</head>
+<body>
 
-		<script src="client/vendor/angular/angular.min.js" type="text/javascript" charset="utf-8"></script>
-		<script src="client/vendor/angular-route/angular-route.min.js" type="text/javascript" charset="utf-8"></script>
-		<script src="client/vendor/angular-ui-router/release/angular-ui-router.min.js" type="text/javascript" charset="utf-8"></script>
-		<script src="client/app/app.js" type="text/javascript" charset="utf-8"></script>
-		<script src="client/app/routes.js" type="text/javascript" charset="utf-8"></script>
-		<script src="client/app/controllers.js" type="text/javascript" charset="utf-8"></script>
-		<script src="client/app/controllers/HomeCtrl.js" type="text/javascript" charset="utf-8"></script>
-		<script src="client/vendor/jquery/dist/jquery.min.js" type="text/javascript" charset="utf-8"></script>
-		<script src="client/vendor/bootstrap/dist/js/bootstrap.min.js" type="text/javascript" charset="utf-8"></script>
-	</body>
+	<div class="container">
+		<div ui-view> </div>
+	</div>
+
+	<script src="client/vendor/angular/angular.min.js" type="text/javascript" charset="utf-8"></script>
+	<script src="client/vendor/angular-route/angular-route.min.js" type="text/javascript" charset="utf-8"></script>
+	<script src="client/vendor/angular-ui-router/release/angular-ui-router.min.js" type="text/javascript" charset="utf-8"></script>
+	<script src="client/app/app.js" type="text/javascript" charset="utf-8"></script>
+	<script src="client/app/routes.js" type="text/javascript" charset="utf-8"></script>
+	<script src="client/app/controllers.js" type="text/javascript" charset="utf-8"></script>
+	<!-- yeoman controllers go here  -->
+	<generated-controllers>
+		<script src="client/app/controllers/homeCtrl.js" type="text/javascript" charset="utf-8"></script>
+	</generated-controllers>
+	<!-- yeoman controllers end here  -->
+	<script src="client/vendor/jquery/dist/jquery.min.js" type="text/javascript" charset="utf-8"></script>
+	<script src="client/vendor/bootstrap/dist/js/bootstrap.min.js" type="text/javascript" charset="utf-8"></script>
+</body>
 </html>
